5ebb46c047be6d6c24017c15dd3f53877a21796d
Eduardo Lange <eduardoandrelange@gmail.com>
2024-05-02 14:28:07 -0300

    até post author

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,4 +1,4 @@
-import { createSlice, nanoid } from '@reduxjs/toolkit'
+import { createSlice, nanoid } from '@reduxjs/toolkit' //nanoID vai importar um randomID
 
 const initialState = [
 	{ id: '1', title: 'Learning reduxtoolkit', content: "I've hear a good story" },
@@ -13,12 +13,13 @@ const postsSlice = createSlice({
 			reducer(state, action) {
 				state.push(action.payload)
 			},
-			prepare(title, content) {
+			prepare(title, content, userId) {
 				return {
 					payload: {
 						id: nanoid(),
 						title,
 						content,
+						userId
 					},
 				}
 			},
