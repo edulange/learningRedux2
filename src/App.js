bac810a1ebd6b542f9b8a56165a6a1def1ae716b
Eduardo Lange <eduardoandrelange@gmail.com>
2024-05-03 20:40:18 -0300

    fetch users

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PostsList from './features/posts/PostsList'
 import AddPostForm from './features/posts/AddPostForm'
 
+
 const App = () => {
   return (
     <main className='App'>
--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,17 +1,26 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
 
- const initialState = [
-    { id: '0', name: 'Dude Lebowski'},
-    { id: '1', name: 'Eduardo Lange'},
-    { id: '2', name: 'Sarana Lange'},
- ]
+const USERS_URL = 'https://jsonplaceholder.typicode.com/users'
 
- const usersSlice = createSlice({
-    name: 'users',
-    initialState,
-    reducers: {}
- })
+const initialState = []
 
- export const selectAllUsers = (state) => state.users
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+	const response = await axios.get(USERS_URL)
+	return response.data
+})
 
- export default usersSlice.reducer
+const usersSlice = createSlice({
+	name: 'users',
+	initialState,
+	reducers: {},
+   extraReducers(builder) {
+      builder.addCase(fetchUsers.fulfilled, (state, action) => {
+         return action.payload
+      })
+   }
+})
+
+export const selectAllUsers = (state) => state.users
+
+export default usersSlice.reducer
