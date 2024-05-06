c129bada156bbd2c2fb43d3dc2a5b9edc539da8c
Eduardo Lange <eduardoandrelange@gmail.com>
2024-05-05 21:04:21 -0300

    some fixing and delete option

--- src/features/posts/TimeAgo.js
+++ src/features/posts/TimeAgo.js
@@ -1,19 +1,17 @@
-import { parseISO, formatDistanceToNow } from "date-fns";
+import { parseISO, formatDistanceToNow } from 'date-fns';
 
-import React from 'react'
-
-const TimeAgo = ({ timestamp}) => {
+const TimeAgo = ({ timestamp }) => {
     let timeAgo = ''
     if (timestamp) {
         const date = parseISO(timestamp)
         const timePeriod = formatDistanceToNow(date)
         timeAgo = `${timePeriod} ago`
     }
-  return (
-    <span title={timestamp}>
-        &nbsp; <i>{timeAgo}</i>
-    </span>
-  )
-}
 
+    return (
+        <span title={timestamp}>
+            &nbsp; <i>{timeAgo}</i>
+        </span>
+    )
+}
 export default TimeAgo
