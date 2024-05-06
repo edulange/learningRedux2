f296ec8a329429501701a356e3c194b3b5c4a305
Eduardo Lange <eduardoandrelange@gmail.com>
2024-05-06 20:01:39 -0300

    users

--- src/features/users/UsersList.js
+++ src/features/users/UsersList.js
@@ -1,9 +1,9 @@
 import { useSelector } from 'react-redux'
-import { selectAllUser } from './usersSlice'
+import { selectAllUsers } from './usersSlice'
 import { Link } from 'react-router-dom'
 
 const UsersList = () => {
-	const users = useSelector(selectAllUser)
+	const users = useSelector(selectAllUsers)
 
 	const renderedUsers = users.map((user) => (
 		<li key={user.id}>
