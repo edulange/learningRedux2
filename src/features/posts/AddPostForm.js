2ee0eae87b03e47bc532bec68f9672b11eb25360
Eduardo Lange <eduardoandrelange@gmail.com>
2024-05-05 21:11:57 -0300

    some fixing

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -2,9 +2,12 @@ import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { addNewPost } from './postsSlice'
 import { selectAllUsers } from '../users/usersSlice'
+import { useNavigate } from 'react-router-dom'
 
 const AddPostForm = () => {
 	const dispatch = useDispatch()
+
+	const navigate = useNavigate()
 
 	const [title, setTitle] = useState('')
 	const [content, setContent] = useState('')
@@ -29,6 +32,7 @@ const AddPostForm = () => {
 				setTitle('')
 				setContent('')
 				setUserId('')
+				navigate('/')
 			} catch (err) {
 				console.error('failed to save the post, escrito no "AddPostForm"', err)
 			} finally {
